ed152f433232163f210157117c5a89340d94ba28
Jianyi_1001 <jianyishi1995@outlook.com>
2019-06-30 19:54:20 -0400

    formatting

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request, \
-    redirect, jsonify, url_for, flash
+    redirect, jsonify, url_for, flash, make_response
 from flask import session as login_session
 
 from sqlalchemy import create_engine
@@ -10,11 +10,9 @@ from database_setup import Category, Item, Base
 
 import random
 import string
-
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
 import httplib2
 import json
-from flask import make_response
 import requests
 
 
@@ -26,7 +24,6 @@ APPLICATION_NAME = "Catalog App"
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 
 
@@ -45,17 +42,13 @@ def show_login():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Obtain authorization code
     code = request.data
-
     try:
-        # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
@@ -65,19 +58,17 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check that the access token is valid.
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'
            .format(access_token))
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
+
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
@@ -85,28 +76,23 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-
     if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(
             json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    # Get user info
     user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(user_info_url, params=params)
@@ -117,29 +103,22 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-    output += '!</h1>'
+    output = '<h1>user name: {}</h1>'.format(login_session['username'])
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;' \
-              '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' "style="width:100px; height:100px; border-radius:100px;' \
+              '-webkit-border-radius:100px; -moz-border-radius:100px;">'
+
     flash("you are now logged in as {}s".format(login_session['username']))
-    print("done!")
     return output
 
 
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
-
     if access_token is None:
         flash('Access Token is None')
         return redirect(url_for('show_catalog'))
-
-    print('In gdisconnect access token is {}'.format(access_token))
-    print('User name is: {}'.format(login_session['username']))
 
     revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
                            params={'token': access_token},
@@ -158,7 +137,7 @@ def gdisconnect():
     return redirect(url_for('show_catalog'))
 
 
-# home page, display categories and each category's latest item
+# home page, display categories and latest items
 @app.route('/')
 @app.route('/catalog/')
 def show_catalog():
