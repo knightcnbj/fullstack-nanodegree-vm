4af99bc0fd959902f7baf14c8709b8991052ae82
Jianyi_1001 <jianyishi1995@outlook.com>
2019-07-06 14:34:52 -0700

    no passwd for psql user

--- vagrant/catalog/adding_items_cate.py
+++ vagrant/catalog/adding_items_cate.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Category, Item, Base
 
 # engine = create_engine('sqlite:///catalog.db')
-engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
+engine = create_engine('postgresql://catalog:@localhost/catalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
