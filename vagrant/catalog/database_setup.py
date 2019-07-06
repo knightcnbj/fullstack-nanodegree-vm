922bdd4b0db2a963e18904edfb273517476693ad
Jianyi_1001 <jianyishi1995@outlook.com>
2019-07-06 14:35:47 -0700

    update passwd

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -50,5 +50,5 @@ class Item(Base):
 
 
 # engine = create_engine('sqlite:///catalog.db')
-engine = create_engine('postgresql://catalog:@localhost/catalog')
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
 Base.metadata.create_all(engine)
